2a303dd9d56c9147676265beab613292fa15f259
ysyoo89 <ysyoo89@10x10.co.kr>
2023-02-24 16:15:33 +0900

    초기 데이터 설정

---
+++ src/main/resources/import.sql
@@ -0,0 +1,25 @@
+INSERT INTO TOP_MENU (top_menu_name, banner) VALUES('옷', '');
+INSERT INTO TOP_MENU (top_menu_name, banner) VALUES('모자', '');
+INSERT INTO TOP_MENU (top_menu_name, banner) VALUES('신발', '');
+
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(1, '상의');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(1, '하의');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(1, '코트');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(2, '트래퍼');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(2, '캡모자');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(3, '축구화');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(3, '농구화');
+INSERT INTO MIDDLE_MENU (top_id, middle_menu_name) VALUES(3, '골프화');
+
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(1, '스웨터');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(1, '맨투맨');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(2, '반바지');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(2, '슬랙스');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(3, '트렌치코트');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(3, '피코트');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(6, '나이키');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(6, '아디다스');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(7, '나이키');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(7, '아디다스');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(8, '에코');
+INSERT INTO BOTTOM_MENU (middle_id, bottom_menu_name) VALUES(8, '나이키');
